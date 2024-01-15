779c186aaa1943a80b117d7b10ce2419c59badf6
mehedihasan <ehasanmunn84@gmail.com>
2024-01-16 02:23:34 +0600

    [DONE] dbURI Changes again

--- src/server.js
+++ src/server.js
@@ -2,24 +2,30 @@
 /* eslint-disable no-console */
 
 const mongoose = require('mongoose');
-const  app= require( "./app");
+const app = require('./app');
+require('dotenv').config();
 
-require("dotenv").config()
 const mongoURI = process.env.MONGODB_URI;
+
 async function bootstrap() {
   try {
-    console.log(typeof(mongoURI));
-    await mongoose.connect(`${mongoURI}`);
-    console.log(`🛢   Database is connected successfully`);
+    console.log(typeof mongoURI); // Logging the type of URI is not necessary
 
-    app.listen(process.env.PORT || 5000 , () => {
-  
-      console.log(`Application  listening on port ${process.env.PORT}`);
+    await mongoose.connect(mongoURI, {
+     
+     
+       // Add this line to prevent deprecation warning
+    });
+
+    console.log('🛢   Database is connected successfully');
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Application listening on port ${port}`);
     });
   } catch (err) {
-    console.error("Failed to connect database", err);
+    console.error('Failed to connect to the database', err);
   }
 }
 
 bootstrap();
-
