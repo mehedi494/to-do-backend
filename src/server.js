c57ad3fc0dcfa19e73515905349f901bb12963de
mehedihasan <ehasanmunn84@gmail.com>
2024-01-16 01:11:40 +0600

    [DONE] Changes db uri

--- src/server.js
+++ src/server.js
@@ -1,22 +1,17 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 
-const mongoose= require("mongoose");
+const mongoose = require('mongoose');
 const  app= require( "./app");
-
-const  config= require( "./config/index");
-// const  { errorlogger } =require ( "./shared/logger");
-// import { RedisClient } from "./shared/redis";
 
 async function bootstrap() {
   try {
-   
-    await mongoose.connect(config.database_url);
-    // logger.info(`🛢   Database is connected successfully`);
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log(`🛢   Database is connected successfully`);
 
-    app.listen(config.port||5000 , () => {
-      // logger.info(`Application  listening on port ${config.port}`);
-      console.log(`Application  listening on port ${config.port}`);
+    app.listen(config.port|| 5000 , () => {
+  
+      console.log(`Application  listening on port ${process.env.PORT}`);
     });
   } catch (err) {
     console.error("Failed to connect database", err);
@@ -25,9 +20,3 @@ async function bootstrap() {
 
 bootstrap();
 
-// process.on('SIGTERM', () => {
-//   logger.info('SIGTERM is received');
-//   if (server) {
-//     server.close();
-//   }
-// });
