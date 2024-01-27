306720dd5122c4558f24f4ed9143ec3ffb175487
mehedihasan <ehasanmunn84@gmail.com>
2024-01-27 22:55:05 +0600

    ✅ Added nested SubMenu

---
+++ src/components/ui/Header/NestedMenu.jsx
@@ -0,0 +1,48 @@
+// NestedMenu.js
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NestedMenu = ({ menuText, subMenu, icon }) => {
+  // const subMenu = [...Submenu];
+  console.log(menuText);
+  console.log(subMenu);
+  console.log(icon);
+
+  const [isSubMenuVisible, setIsSubMenuVisible] = useState(false);
+
+  const handleHover = () => {
+    setIsSubMenuVisible(true);
+  };
+
+  const handleLeave = () => {
+    setIsSubMenuVisible(false);
+  };
+  return (
+    <div
+      className="relative group p-2 hidden md:hidden lg:hidden xl:inline  sm:hidden"
+      onMouseEnter={handleHover}
+       onMouseLeave={handleLeave}
+    >
+      <span className="cursor-pointer hover:text-[#fc690b] transition duration-200">
+        {menuText}
+        {icon}
+      </span>
+      {isSubMenuVisible && (
+        <ul className=" absolute flex  mt-2 flex-col gap-3 p-3 transition hover:duration-500  rounded-xl border-gray-400    bg-[#4A4A4A]   border" style={{ width: 'max-content' }}>
+          {/* Render your sub-menu options here */}
+          {subMenu.map((item, index) => (
+            <Link to={item?.link}>
+              <li 
+                key={index}
+                className="flex items-center hover:text-[#fc690b] transition duration-200 px-3 space-x-2">
+             <span className="">{item.preIcon}</span> <span>{item?.subMenuText}</span>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NestedMenu;
