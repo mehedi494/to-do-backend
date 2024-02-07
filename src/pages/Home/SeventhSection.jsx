04177b74c92714047e483b4f207af7964f22ab6d
mehedi494 <ehasanmunna84@gmail.com>
2024-02-07 20:20:59 +0600

    change card t

--- src/pages/Home/SeventhSection.jsx
+++ src/pages/Home/SeventhSection.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import exo_sys1 from "../../assets/ecosystem-1.png";
-import exo_sys2 from "../../assets/ecosystem-2.png";
-import exo_sys3 from "../../assets/ecosystem-3.png";
+import exo_sys1 from "../../assets/What-Is-Staking-400x300.png";
+import exo_sys2 from "../../assets/payment.png";
+import exo_sys3 from "../../assets/RWA.png";
 
 export default function SeventhSection() {
   const cardContent = [
     {
       img: exo_sys1,
-
-      description:
-        "Embark on epic narratives and dynamic adventures, where you can create, customize, and control unique characters.",
-      catagories: "RPGs",
+      catagories: "Staking Program (20 Years) ",
+      list: [
+        "1. Stake BNB will get NAN0",
+        "2. Stake BTC will get NAN0",
+        "3. Stake NAN0 will get NAN0",
+      ],
     },
     {
       img: exo_sys2,
-
-      description:
-        "Engage in thrilling battles where strategy and skill collide for intense one-on-one or team-based showdowns.",
-      catagories: "Fighting",
+      catagories: "Payment System",
+      list: ["1. Mobile App", "2. P2P/Online", "3. In-store/Offline"],
     },
     {
       img: exo_sys3,
-
-      description:
-        "Designed to entertain and stimulate, these games offer a delightful blend of fun and cognitive engagement.",
-      catagories: "Puzzle",
+      catagories: "Real World Asset",
+      list: ["1. Real Estate", "2. Vechicle", "3. Gold/NFT"],
     },
   ];
   return (
@@ -34,25 +32,22 @@ export default function SeventhSection() {
           <h1 className="text-xl sm:text-5xl font-semibold text-[#333333]">
             Explore Bitcoinnano Ecosystem
           </h1>
-          <p className="text-sm sm:text-md text-[#333333]">
-            Bitcoinnano is leading the charge in constructing a versatile gaming
-            ecosystem for the Bitcoin Blockchain, catering to each individual's
-            unique preferences.
-          </p>
         </div>
         {/* card */}
         <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2  gap-4 justify-items-center items-center">
           {/* maping on card */}
-          {cardContent.map((card, index) => (
-            <div className="flex flex-col gap-y-2 max-w-[350px] max-h-[600px] bg-white border shadow-md p-4 rounded-xl">
+          {cardContent?.map((card, index) => (
+            <div key={index} className="flex flex-col gap-y-2 max-w-[350px] max-h-[600px] bg-white border shadow-md p-4 rounded-xl">
               <h1 className="text-center text-2xl font-semibold">
                 {card?.catagories}
               </h1>
-              <img src={card?.img} alt="rpgs" />
-              <p>{card?.description}</p>
-              <button className="flex justify-between  my-2 bg-gradient-to-r from-[#fc690b] to-[#feab2d] hover:opacity-80 rounded-lg text-white items-center  p-4 gap-x-4 transition-transform transform hover:scale-95">
+              <img className="rounded" src={card?.img} alt="rpgs" />
+             {
+              card?.list.map((item,index)=>( <p className="text-cente P-4 text-xl font-semibold text-[#feab2d]"> {item}</p>))
+             }
+              {/* <button className="flex justify-between  my-2 bg-gradient-to-r from-[#fc690b] to-[#feab2d] hover:opacity-80 rounded-lg text-white items-center  p-4 gap-x-4 transition-transform transform hover:scale-95">
                 Learn more
-              </button>
+              </button> */}
             </div>
           ))}
         </div>
